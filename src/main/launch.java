27c5bcac440deb754ad067b1c03178a80721de6f
Brandon Whitfield <whitfield.b89@gmail.com>
2014-07-27 10:57:54 -0500

    Log4J works now, still in a spike-ish mode though - BW

--- src/main/launch.java
+++ src/main/launch.java
@@ -17,59 +17,22 @@ import util.ICommander;
 public class launch {
 
 	public static void main(String[] args) {
-		Logger logger = LogManager.getLogger(); //create an application wide driver
-//		ICommander commander = new Commander();
-//		ICommandFactory cf = new CommandFactory(commander);
-		logger.error("printing things");
-//		try {
-//			IDefaultInformation di = new DefaultInformation(commander, logger);
-//			di.print();
-//			while (true) {
-//				cf.obd2Value("01", Commands.RPM);
-//				cf.obd2Value("01", Commands.LOAD);
-//				cf.obd2Value("01", Commands.MAF);
-////				getRpms(commander);
-////				getLoad(commander);
-////				getMAF(commander);
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-	}
-
-	private static void getRpms(ICommander commander) throws IOException {
-		String response = commander.obd2("01", "0C");
-		response = response.replace("41", "");
-		response = response.replace("0C", "");
-		response = response.replace(" " , "");
-		if (response.length() > 0 && !response.contains("SEARCH")) {
-			int decVal = Integer.parseInt(response, 16);
-			int rpm = decVal / 4;
-			System.out.println("RPM: " + rpm);// get rpms
-		}
-	}
-	
-	private static void getLoad(ICommander commander) throws IOException {
-		String response = commander.obd2("01", "04");
-		response = response.replace("41", "");
-		response = response.replace("04", "");
-		response = response.replace(" " , "");
-		if (response.length() > 0 && !response.contains("SEARCH")) {
-			int decVal = Integer.parseInt(response, 16);
-			double loadPercent = (decVal * 100) / 255;
-			System.out.println("Load: " + loadPercent + "%");// get load
-		}
-	}
-	
-	private static void getMAF(ICommander commander) throws IOException {
-		String response = commander.obd2("01", "10");
-		response = response.replace("41", "");
-		response = response.replace("10", "");
-		response = response.replace(" " , "");
-		if (response.length() > 0 && !response.contains("SEARCH")) {
-			int decVal = Integer.parseInt(response, 16);
-			double mafGramPerSec = decVal / 100;
-			System.out.println("MAF: " + mafGramPerSec + " gram/sec");// get load
+		Logger logger = LogManager.getLogger(); //create ROOT logger
+		logger.info("JOBD2WifiDriver - Begin");
+		
+		ICommander commander = new Commander();
+		ICommandFactory cf = new CommandFactory(commander);
+		try {
+			IDefaultInformation di = new DefaultInformation(commander, logger);
+			di.print();
+			while (true) {
+				logger.info("RPM: " + cf.obd2Value("01", Commands.RPM));
+				logger.info("LOAD: " + cf.obd2Value("01", Commands.LOAD));
+				logger.info("MAF: " + cf.obd2Value("01", Commands.MAF));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e.getStackTrace());
 		}
 	}
 }

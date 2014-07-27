44d363a0b4fcccb801f4f3f98ba806a66fe9f8ff
Brandon Whitfield <whitfield.b89@gmail.com>
2014-07-27 11:35:54 -0500

    refactor - BW

--- src/logic/EcuMath.java
+++ src/logic/EcuMath.java
@@ -3,24 +3,26 @@ package logic;
 public class EcuMath implements IEcuMath {
 
 	public String rpm(String cleanEcuHex) {
-		int decVal = Integer.parseInt(cleanEcuHex, 16);
-		double computedValue = decVal / 4.0;
-		double roundedVal = (double) Math.round(computedValue * 100) / 100;
-		return String.valueOf(roundedVal);
+		double computedValue = hexToDecimal(cleanEcuHex) / 4.0;
+		return round(computedValue);
 	}
 
 	public String load(String cleanEcuHex) {
-		int decVal = Integer.parseInt(cleanEcuHex, 16);
-		double computedValue = (decVal * 100) / 255.0;
-		double roundedVal = (double) Math.round(computedValue * 100) / 100;
-		return String.valueOf(roundedVal);
+		double computedValue = (hexToDecimal(cleanEcuHex) * 100) / 255.0;
+		return round(computedValue);
 	}
 
 	public String maf(String cleanEcuHex) {
-		int decVal = Integer.parseInt(cleanEcuHex, 16);
-		double computedValue = decVal / 100.0;
+		double computedValue = hexToDecimal(cleanEcuHex) / 100.0;
+		return round(computedValue);
+	}
+
+	private int hexToDecimal(String cleanEcuHex) {
+		return Integer.parseInt(cleanEcuHex, 16);
+	}
+	
+	private String round(double computedValue) {
 		double roundedVal = (double) Math.round(computedValue * 100) / 100;
 		return String.valueOf(roundedVal);
 	}
-
 }
